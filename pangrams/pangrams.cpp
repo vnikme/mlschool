658520ce39bfb1ebcdfa4f6d635dab3b65c40d72
Vadim Nikulin <accs@vnik.me>
2022-07-08 21:45:02 +0100

    Switch to std::function.

--- pangrams/pangrams.cpp
+++ pangrams/pangrams.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <locale>
 #include <string>
@@ -9,20 +11,20 @@
 const std::string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
 
-template<typename TFunc>
-void RunAndMeasure(const TFunc &func, int count, const std::string &text) {
+void ToLower(std::string &text) {
+    std::locale locale;
+    std::transform(text.begin(), text.end(), text.begin(), [&locale](char sym) { return std::tolower(sym, locale); });
+}
+
+
+void RunAndMeasure(const std::function<bool(const std::string &)> &func, int count, std::string text) {
+    ToLower(text);
     std::cout << (func(text) ? "true" : "false") << ' ';
     std::clock_t start = std::clock();
     for (int i = 0; i < count; ++i)
         func(text);
     std::clock_t finish = std::clock();
     std::cout << (finish - start) << std::endl;
-}
-
-
-void ToLower(std::string &text) {
-    std::locale locale;
-    std::transform(text.begin(), text.end(), text.begin(), [&locale](char sym) { return std::tolower(sym, locale); });
 }
 
 
@@ -38,7 +40,6 @@ bool CheckForEveryDigit(const std::string &text) {
 
 
 bool CheckForEveryDigitWithoutLower(const std::string &text) {
-    //ToLower(text);
     for (char sym : ALPHABET) {
         if (text.find(sym) == std::string::npos)
             return false;
@@ -48,10 +49,9 @@ bool CheckForEveryDigitWithoutLower(const std::string &text) {
 
 
 bool CheckViaFlags(const std::string &text) {
-    //ToLower(text);
     int flags = 0;
     for (char sym : text) {
-        unsigned char code = int(sym) - int('a');
+        unsigned char code = sym - 'a';
         if (code < 26)
             flags |= (1 << code);
     }
@@ -59,30 +59,11 @@ bool CheckViaFlags(const std::string &text) {
 }
 
 
-bool CheckViaFlagsImproved(const std::string &text) {
-    //ToLower(text);
-    int trueCount = 0;
-    bool flags[26] = {};
-    for (char sym : text) {
-        int code = int(sym) - int('a');
-        if (0 <= code && code < 26) {
-            if (!flags[code]) {
-                flags[code] = true;
-                ++trueCount;
-            }
-        }
-    }
-    return (trueCount == 26);
-}
-
-
-
 void RunAll(int count, const std::string &text) {
     std::cout << "Text size: " << text.size() << std::endl;
     RunAndMeasure(CheckForEveryDigit, count, text);
     RunAndMeasure(CheckForEveryDigitWithoutLower, count, text);
     RunAndMeasure(CheckViaFlags, count, text);
-    RunAndMeasure(CheckViaFlagsImproved, count, text);
     std::cout << std::endl;
 }
 
@@ -91,8 +72,13 @@ int main() {
     RunAll(10000, "Jackdaws love my big sphinx of quartz");
     RunAll(10000, "Jackdaws love my sphinx of quartz");
     std::string text;
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < 1000000; ++i) {
         text += ' ';
+        if (rand() % 2 == 0)
+            text += '.';
+        else
+            text += ',';
+    }
     for (int i = 0; i < ALPHABET.size(); ++i) {
         for (int j = 10; j > 0; --j) {
             text += ALPHABET[i];
--- pangrams/run.sh
+++ pangrams/run.sh
@@ -1,4 +1,4 @@
-g++ --std=c++11 pangrams.cpp
+g++ --std=c++11 -O3 pangrams.cpp
 ./a.out
 rm a.out
 
